02aeb590124e69c96187067866ab2a1ac68326d1
louis <louispayne10@hotmail.co.uk>
2022-08-17 19:06:48 +0100

    update rofi stuff

---
+++ dotfiles/.config/rofi/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+menu="$1"
+
+if [ "$menu" = "appmenu" ]; then
+    rofi -modi 'drun,window,filebrowser' -theme appmenu -show 
+elif [ "$menu" = "powermenu" ]; then
+    rofi -modi 'Powermenu:~/.config/rofi/powermenu.sh' -show Powermenu -theme powermenu
+elif [ "$menu" == "emoji" ]; then
+    rofimoji --selector-args "-theme emoji"
+else
+    echo 'unrecognised command'
+fi
